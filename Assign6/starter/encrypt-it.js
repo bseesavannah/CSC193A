b9c1849e1873d177aa806797d35a7143076fe8bd
Savannah Birdsong-See <bseesavannah@gmail.com>
2024-10-24 00:04:18 -0700

    shift cipher algorithm implemented I used the sample solution provided in the hint section

--- Assign6/starter/encrypt-it.js
+++ Assign6/starter/encrypt-it.js
@@ -28,17 +28,50 @@
     resetButton.addEventListener("click", handleReset);
   }
 
+  // Add any other functions in this area (you should not implement your
+  // entire program in the init function, for similar reasons that
+  // you shouldn't write an entire Java program in the main method).
+
   function handleClick() {
-    console.log("Button clicked!"); // When a button is clicked, log it
+    const inputTextArea = document.getElementById("input-text");
+    const output = document.getElementById("result");
+
+    // Grab the text to be encrypted
+    const textToEncrypt = inputTextArea.value;
+
+    // Encrypt the text
+    const encryptedText = shiftCipher(textToEncrypt);
+
+    // When the Encrypt-it button is clicked, log it
+    console.log("Button clicked!"); 
+
+    output.innerText = encryptedText;
   }
 
   function handleReset() {
     const inputTextArea = document.getElementById("input-text");
     inputTextArea.value = ""; // Clear contents of the box
+    console.log("Box Reset");
   }
 
-  // Add any other functions in this area (you should not implement your
-  // entire program in the init function, for similar reasons that
-  // you shouldn't write an entire Java program in the main method).
+
+  function shiftCipher(text) {
+    text = text.toLowerCase();
+    let result = "";
+
+    for(let i = 0; i < text.length; i++) {
+      // If the character is not a-z, don't change it
+      if(text[i] < 'a' || text[i] > 'z') {
+        result += text[i];
+      }
+      else if(text[i] === 'z') {
+        result += 'a'; // Wrap-around to the start of the alphabet
+      }
+      else { // Character is a-y
+        result += String.fromCharCode(text.charCodeAt(i) + 1);
+      }
+    }
+    return result;
+  }
 
 })();
