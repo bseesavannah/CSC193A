fa2d3795194515b185e17bd043825ed988144fd2
Savannah Birdsong-See <bseesavannah@gmail.com>
2024-10-23 22:53:38 -0700

    When 'Bigger' is clicked, the text area becomes larger and the font size increases to 24pt

--- Assign5/fancifymytext.js
+++ Assign5/fancifymytext.js
@@ -1,3 +1,10 @@
 const showAlert = () => {
-    alert("Hello, world!");
+    // The Alert
+    //alert("Hello, world!");
+
+    // Get the textarea element by ID
+    const textarea = document.getElementById("myTextArea");
+    
+    // Change font size
+    textarea.style.fontSize = "24pt";
 }
