c3ff7ff4aba54b8f60853cfaef8d3a7c3ae26a7f
Savannah Birdsong-See <bseesavannah@gmail.com>
2024-10-23 23:11:19 -0700

    Added event handler for FancyShmancy button to change text, while BoringBetty changes it back

--- Assign5/fancifymytext.js
+++ Assign5/fancifymytext.js
@@ -1,10 +1,35 @@
 const showAlert = () => {
     // The Alert
-    //alert("Hello, world!");
+    alert("Hello, world!");
+}
 
+const changeSize = () => {
     // Get the textarea element by ID
     const textarea = document.getElementById("myTextArea");
     
     // Change font size
     textarea.style.fontSize = "24pt";
+}
+
+const biggerClick = () => {
+    showAlert();
+    changeSize();
+}
+
+const changeStyle = () => {
+    // Get the textarea element by ID
+    const textarea = document.getElementById("myTextArea");
+
+    // Logic for which button is clicked
+    if (document.getElementById("fancyShmancy").checked) {
+        // Apply styles
+        textarea.style.fontWeight = "bold";
+        textarea.style.color = "blue";
+        textarea.style.textDecoration = "underline";
+    }
+    else if(document.getElementById("boringBetty").checked) {
+        textarea.style.fontWeight = "normal";
+        textarea.style.color = "";
+        textarea.style.textDecoration = "none";
+    }
 }
